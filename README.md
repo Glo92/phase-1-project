a21921811f4822702ec400eb721ce05d05c6532d
Gloria Wekura <wekuraglorian07@gmail.com>
2024-04-12 22:47:01 +0300

    submit project

---
+++ README.md
@@ -0,0 +1,36 @@
+# THE TO DO LIST APP 
+
+#### Date, 2024/04/12
+
+#### By *Gloria Wekura*
+
+## Description
+This is an app the the user is able to input the tasks that he/ she is required to complete and once they are done the can mark as done,or delete them 
+
+## Installation
+You use git clone to be able to download the app in site Github
+
+## Installation Requirements
+Git
+
+### Installation instruction
+```
+Git clone git@github.com:Glo92/phase-1-project.git
+
+```
+
+# Live Link
+
+
+## Technologies used
+Javascript 
+HTML
+CSS
+GitHub 
+
+## Support and contact details
+github.com/Glo92
+
+### License
+The content of this site is licensed under the MIT license
+Copyright (c) 2024.
---
+++ js/index.js
@@ -0,0 +1,69 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+// fetch tasks
+fetch('http://localhost:3000/Tasks')
+.then((data) => data.json())
+.then(data => console.log( data))
+
+const input = document.getElementById('inputItem')
+const addButton = document.getElementById('addbtn')
+const listContainer = document.getElementById('list-container')
+
+//make the add button work
+function button() {
+addButton.addEventListener('click', () => {
+    if(input.value !== "") {
+        //create li element
+        const list = input.value
+        const li = document.createElement('li')
+        li.innerHTML = `
+          ${list}
+        <button class="delete">Delete</button>
+        `
+       
+        //append the li 
+        listContainer.appendChild(li)
+        input.value = ""
+        saveTasks()
+        
+        
+    }
+})
+}
+button()
+//anabling the delete button to delete tasks
+function tasklist() {
+    document.addEventListener('click', (e) => {
+        if(e.target.classList.contains('delete')) {
+            const del = e.target.parentElement
+            listContainer.removeChild(del)
+            saveTasks()
+        }
+
+    })
+}
+tasklist()
+//save task to the storagw
+function saveTasks() {
+    localStorage.setItem('data',listContainer.innerHTML)
+}
+//upload in the storage
+function uploadTasks() {
+    listContainer.innerHTML = localStorage.getItem('data')
+}
+uploadTasks()
+//mark tasks as completed
+function completed() {
+    listContainer.addEventListener('click', (e) => {
+        if(e.target.tagName === 'li') {
+            e.target.classList.toggle('checked')
+        } 
+
+        
+
+    })
+}
+completed()
+
+
+})
